790b600eb8ab1e6b056e7aeb05203e59b523b83a
Dino <jl_lsj@163.com>
2015-06-12 17:24:27 +0800

    updated

--- d/s/dist/controllers.js
+++ d/s/dist/controllers.js
@@ -16,10 +16,7 @@ app.controller('ListController', function($rootScope, $scope, $routeParams){
   // 
   // 'Scroll' screen
   // 
-  var scrollItems = [ 
-      {id: 1, question: '综合题', answer: [{a:'dfsdf fdfd', b:'dfd fdf erer'}]},
-      {id: 2, question: '综合知识', answer: [{a:'dfsdf fdfd', b:'dfd fdf erer'}]},
-  ];
+  var scrollItems = Categories;
   
   if($routeParams.type === '1'){
     self.titleContent = '刷题';
@@ -42,32 +39,21 @@ app.controller('ListController', function($rootScope, $scope, $routeParams){
   });
 });
 
-app.controller('TestController', function($rootScope, $scope, $routeParams, $location, SharedState){
+app.controller('TestController', function($rootScope, $scope, $routeParams, $location, dataService){
   var self = this;
   self.titleContent = '请选择一个答案';  
-  var scrollItems = [ 
-      {id: 1, question: '“铁观音”茶的原产发源地在中国的哪个省份？', answer: ['福建', '安徽'],right:'福建'},
-      {id: 2, question: '奢侈品牌 LV 路易威登是哪个国家的品牌？', answer: ['法国', '英国'],right:'福建'},
-      {id: 3, question: '欧洲的哪个城市被世界公认为“雾都”', answer: ['伦敦', '巴黎'],right:'伦敦'},
-      {id: 4, question: '西藏布达拉宫最初是松赞干布为了迎接谁而兴建的？', answer: ['文成公主', '铁扇公主'],right:'文成公主'},
-      {id: 5, question: '电影《碟中谍 4》中汤姆布鲁斯挑战的世界第一高的摩天大楼位于哪个城市？', answer: ['迪拜', '英国'],right:'迪拜'},
-      {id: 6, question: '电影《变形金刚》中，与擎天柱作对的反派首领叫什么？', answer: ['威震天', '大黄蜂'],right:'威震天'},
-      {id: 7, question: '金庸小说《射雕英雄传》中第一次华山论剑的胜利者是哪一位？', answer: ['王重阳', '郭靖'],right:'王重阳'},
-      {id: 8, question: '法国巴黎有名的凯旋门是为了纪念哪位名人建造的？', answer: ['拿破仑', '希特勒'],right:'拿破仑'},
-      {id: 9, question: '世界上出现麦田怪圈最多的国家是', answer: ['法国', '英国'],right:'福建'},
-      {id: 10, question: '意大利有一座被活火山活埋的城市，叫什么？', answer: ['庞贝', '圣彼得堡'],right:'庞贝'},
-  ];
+  var scrollItems = Questions;
   self.index = 0;
-  self.result = 1;
+  self.result = 0;
   self.chooseAnwser = function(x,y,z){
       if(x == y.right){
           self.result++;
       }
       if(self.index === scrollItems.length-1){
           //window.alert('正确回答'+self.result+'题');
-		  SharedState.initialize($scope, 'name');
-		  SharedState.initialize($scope, 'score');
-		  SharedState.setMany({name:'dino', score: 8});
+		  var score = {};
+		  score.reuslt = self.result;
+		  dataService.dataObj = score;
 		  $location.path('result');
       }
       self.index++;
@@ -85,10 +71,10 @@ app.controller('TestController', function($rootScope, $scope, $routeParams, $loc
 });
 
 
-app.controller('ResultController', function($rootScope, $scope, $routeParams, $location, SharedState){
+app.controller('ResultController', function($rootScope, $scope, $routeParams, $location, dataService){
     var self = this;
-    self.result = {total: 8}
-    
+    self.result = dataService.dataObj.reuslt;
+    console.log(dataService.dataObj);
 	self.continuePlay = function(){
 	    $location.path('/test/1/');
 	};
---
+++ d/s/dist/services.js
@@ -0,0 +1,6 @@
+app.factory('dataService', function() {
+  var _dataObj = {};
+  return {
+    dataObj: _dataObj
+  };
+})
